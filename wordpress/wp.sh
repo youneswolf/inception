1a6645c59fa2d064436e57d05ee247cb2e581d3a
Younes Bellakridi <ybellakr@e3r10p12.1337.ma>
2025-02-09 20:26:22 +0100

    wordpress docker file

---
+++ wordpress/wp.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+cd /usr
+
+wget https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
+
+chown -R root:root /var/www/wordpress
+
+#change the line in the config to all inerfaces  (for ngnix)
+
+chmod +x wp-cli.phar
+
+mv wp-cli.phar /usr/local/bin/wp
+
+wp core download --allow-root
+
+wp config create --dbname=wordpress --dbuser=wp_user --dbpass=password --dbhost=localhost --dbprefix=wp_ --allow-root
+
+wp core install --url=localhost --title=Title --admin_user=wp_user --admin_password=password --admin_email=test@email.com --allow-root
+
+php7.4-fpm -F
