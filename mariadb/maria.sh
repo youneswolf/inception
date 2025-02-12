5584178a973672453f002005ff900288c3d93c77
Younes Bellakridi <ybellakr@e3r11p10.1337.ma>
2025-02-12 10:31:41 +0100

    inception

---
+++ mariadb/maria.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+service mariadb start 
+
+sleep 5
+
+mysql -u root -e "CREATE DATABASE wordpress;"
+
+mysql -u root -e "CREATE USER 'wp_user';"
+
+mysql -u root -e "GRANT ALL PRIVILEGES ON wordpress.* TO 'wp_user'@'%' IDENTIFIED BY 'password';"
+
+mysql -u root -e "FLUSH PRIVILEGES;"
+
+mysql -u root -e "ALTER USER 'root'@'localhost' IDENTIFIED BY 'root';"
+
+mysql -u root -p"root" -e "FLUSH PRIVILEGES;"
+
+mysqladmin -u root -proot shutdown
+
+exec mysqld_safe --bind-address=0.0.0.0
+
--- wordpress/wp.sh
+++ wordpress/wp.sh
@@ -1,23 +1,32 @@
 #!/bin/bash
 
-#change the line in the config to all inerfaces  (for ngnix)
+# sed -i 's#listen = /run/php/php7.4-fpm.sock#listen = 0.0.0.0:9000#' /etc/php/7.4/fpm/pool.d/www.conf
 
-wget https://fr.wordpress.org/wordpress-6.0-fr_FR.tar.gz -P /var/www
-
-cd /var/www && tar -xzf wordpress-6.0-fr_FR.tar.gz && rm wordpress-6.0-fr_FR.tar.gz
-
-chown -R root:root /var/www/wordpress
 
 wget https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
-
 chmod +x wp-cli.phar
-
 mv wp-cli.phar /usr/local/bin/wp
+
+mkdir -p /var/www/wordpress
+chown -R www-data:www-data /var/www/wordpress
+cd /var/www/wordpress
 
 wp core download --allow-root
 
-wp config create --dbname=wordpress --dbuser=wp_user --dbpass=password --dbhost=localhost --dbprefix=wp_ --allow-root
+mv /var/www/wordpress/wp-config-sample.php /var/www/wordpress/wp-config.php
+
+wp config set DB_NAME wordpress --allow-root
+wp config set DB_USER wp_user --allow-root
+wp config set DB_PASSWORD password --allow-root
+wp config set DB_HOST mariadb:3306 --allow-root
+wp config set DB_PREFIX wp_ --allow-root
 
 wp core install --url=localhost --title=Title --admin_user=wp_user --admin_password=password --admin_email=test@email.com --allow-root
 
-php7.4-fpm -F
+mkdir -p /run/php
+chown -R www-data:www-data /var/www/wordpress
+
+wp db check --allow-root
+
+/usr/sbin/php-fpm7.4 -F
+
