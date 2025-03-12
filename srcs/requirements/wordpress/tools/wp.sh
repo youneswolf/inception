df6089dfe863995d877acc4ea0a1a063f5906351
youneswolf <123373618+youneswolf@users.noreply.github.com>
2025-03-12 12:38:05 +0000

    ¨inception¨

--- srcs/requirements/wordpress/tools/wp.sh
+++ srcs/requirements/wordpress/tools/wp.sh
@@ -2,6 +2,7 @@
 
 DB_PASSWORD=$(cat /run/secrets/db_password)
 ADMIN_PASSWORD=$(cat /run/secrets/credentials)
+user2=$(cat /run/secrets/user_password.txt)
 
 
 wget https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar >/dev/null 2>&1
@@ -22,11 +23,11 @@ wp config set DB_PASSWORD ${DB_PASSWORD} --allow-root
 wp config set DB_HOST ${host} --allow-root
 wp config set DB_PREFIX ${prefix} --allow-root
 
-wp core install --url=https://ybellakr.42.fr --title=${title} --admin_user=${admin_user} --admin_password=${ADMIN_PASSWORD} --admin_email=${email} --allow-root
+wp core install --url=${url} --title=${title} --admin_user=${admin_user} --admin_password=${ADMIN_PASSWORD} --admin_email=${email} --allow-root
 
 mkdir -p /run/php
 chown -R www-data:www-data /var/www/wordpress
-wp user create ybellakr ybellakr@example.com --role=editor --user_pass=${user2} --allow-root
+wp user create ${user22} ${user22_email} --role=editor --user_pass=${user2} --allow-root
 wp db check --allow-root
 
 exec /usr/sbin/php-fpm7.4 -F
